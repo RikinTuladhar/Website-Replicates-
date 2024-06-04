6dee0d26e37b50676b82b4c845d96be89b4c79cc
Rikin Tuladhar <110732947+RikinTuladhar@users.noreply.github.com>
2024-06-04 16:18:02 +0545

    Update NavBar.jsx

--- Vet Page/src/component/NavBar.jsx
+++ Vet Page/src/component/NavBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 const NavBar = () => {
   return (
-    <div className="sticky border-b-2 shadow-lg top-0 z-50 w-full h-[101.998px] bg-[#f5fafa] flex justify-between px-24 items-center">
+    <div className="sticky border-b-2 shadow-lg top-0 z-50 w-full h-[101.998px] bg-[#f5fafa] flex justify-between px-5 md:px-24 items-center">
       <div className="text-3xl">Your Logo</div>
       <div>
         <GiHamburgerMenu  className="size-6"/>
